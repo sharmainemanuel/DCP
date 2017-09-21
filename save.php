20eb594446b8af4219933edfac2b92914f454759
sharmainemanuel <manuelsharmaine@gmail.com>
2017-09-21 16:37:05 +0800

    1.0.0
    beta testing

---
+++ save.php
@@ -0,0 +1,36 @@
+<?php
+include("config.php");
+	$jo = $_POST["job_order"];
+	$requested_by = $_POST["requested_by"];
+	$office = $_POST["office"];
+	$school = $_POST["school"];
+
+	$issue_cat = $_POST["issue_cat"];
+	$issues = $_REQUEST["issues"];
+
+	//$main_issue = $_POST["main_issue"];
+	//$other_issue = $_POST["other_issue"];
+	//$client = $_POST["client"];
+
+
+	$requestDate = date("Y-m-d H:i:s");
+
+$sql = "INSERT INTO tbl_techassist (fld_id, fld_jobOrder, fld_requestBy,fld_issue,fld_issueCat,fld_status,fld_deleted,fld_office,fld_school,fld_requestDate)
+	VALUES ('', '$jo', '$requested_by','$issues','$issue_cat','0','0','$office','$school','$requestDate')";
+
+	if (mysqli_query($conn, $sql)) {
+	    echo "New record created successfully!";
+	    echo "Job order: " .$jo."\n Request :".$requested_by."\n Office: ".$office;
+		echo "\nSchool: " .$school."\n Category:".$issue_cat."\n issues: ".$issues;
+	} else {
+	    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+
+
+
+
+
+	mysqli_close($conn);
+	
+
+?>
