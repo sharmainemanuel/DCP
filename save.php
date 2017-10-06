9d663de1aa08f58771cb789cf2b04c5034f49068
sharmainemanuel <manuelsharmaine@gmail.com>
2017-10-06 16:08:06 +0800

    test
    test

--- save.php
+++ save.php
@@ -8,15 +8,15 @@ include("config.php");
 	$issue_cat = $_POST["issue_cat"];
 	$issues = $_REQUEST["issues"];
 
-	//$main_issue = $_POST["main_issue"];
-	//$other_issue = $_POST["other_issue"];
-	//$client = $_POST["client"];
+	$main_issue = $_POST["main_issue"];
+	$other_issue = $_POST["other_issue"];
+	$client = $_POST["client"];
 
 
 	$requestDate = date("Y-m-d H:i:s");
 
-$sql = "INSERT INTO tbl_techassist (fld_id, fld_jobOrder, fld_requestBy,fld_issue,fld_issueCat,fld_status,fld_deleted,fld_office,fld_school,fld_requestDate)
-	VALUES ('', '$jo', '$requested_by','$issues','$issue_cat','0','0','$office','$school','$requestDate')";
+$sql = "INSERT INTO tbl_techassist (fld_id, fld_jobOrder, fld_requestBy,fld_issue,fld_issueCat,fld_status,fld_deleted,fld_office,fld_school,fld_requestDate,fld_description,fld_otherissue,fld_client)
+	VALUES ('', '$jo', '$requested_by','$issues','$issue_cat','0','0','$office','$school','$requestDate','$main_issue','$other_issue','$client')";
 
 	if (mysqli_query($conn, $sql)) {
 	    echo "New record created successfully!";
